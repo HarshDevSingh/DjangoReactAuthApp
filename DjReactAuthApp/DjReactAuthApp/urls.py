ba9480305392b7d1995112bdea4bcc9b8ab4bc15
Harsh <Harshdevsingh1992@gmail.com>
2021-02-07 17:46:47 +0000

    added frontend app+integrated react setup

--- DjReactAuthApp/DjReactAuthApp/urls.py
+++ DjReactAuthApp/DjReactAuthApp/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('users.urls'))
+    path('', include('frontend.urls')),
+    path('',include('users.urls')),
 ]
 
 if settings.DEBUG:
