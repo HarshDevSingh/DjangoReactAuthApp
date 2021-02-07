d91f20ab0538a62fc45344a7c80e4b1206112e42
Harsh <Harshdevsingh1992@gmail.com>
2021-02-07 15:42:31 +0000

    improved exeption handeling in user views(for user list+user detail)

--- DjReactAuthApp/users/views.py
+++ DjReactAuthApp/users/views.py
@@ -4,27 +4,32 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
+from django.shortcuts import get_object_or_404
+
 
 # Create your views here.
 @api_view(['GET'])
 def users_list(request):
-    if request.method == 'GET':
-        try:
+    try:
+        if request.method == 'GET':
             users = CustomUser.objects.all()
             serializers = CustomUserSerializer(users, many=True)
-            return Response(serializers.data,status=status.HTTP_200_OK)
-        except:
-            return Response({"error":serializers.errors},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response(serializers.data, status=status.HTTP_200_OK)
+    except:
+        return Response({"error": "something went wrong"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @api_view(['GET'])
 def user_detail(request, id=None):
-    if request.method == 'GET':
-        if id is not None:
-            try:
-                user = CustomUser.objects.get(id=id)
-                serializers = CustomUserSerializer(user, many=False)
-                return Response(serializers.data, status=status.HTTP_200_OK)
-            except:
-                return Response({"error": serializers.errors}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
+    try:
+        if request.method == 'GET':
+            if id is not None:
+                try:
+                    user = get_object_or_404(CustomUser, id=id)
+                    if user:
+                        serializers = CustomUserSerializer(user, many=False)
+                        return Response(serializers.data, status=status.HTTP_200_OK)
+                except:
+                    return Response({'error': 'user not found'}, status=status.HTTP_404_NOT_FOUND)
+    except:
+        return Response({"error": "something went wrong"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
